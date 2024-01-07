41230318431cc01fb1c55e9403b11b6afabf4399
Martijn de Vos <mdmartijndevos@gmail.com>
2024-01-07 16:52:33 +0100

    Updated test_shared_memory script

--- scripts/test_shared_memory.py
+++ scripts/test_shared_memory.py
@@ -7,12 +7,10 @@ from dasklearn.models import create_model
 torch.multiprocessing.set_sharing_strategy('file_system')
 
 print("Creating models in shared memory...")
-models = []
 for i in range(500):
     print("Creating model %d" % i)
     model = create_model("cifar10", architecture="resnet18")
     model = model.share_memory()
-    models.append(model)
-
-print("Sleeping...")
-sleep(60)
+    sleep(0.5)
+    print("Deleting model %d" % i)
+    del model
