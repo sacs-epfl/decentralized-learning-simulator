1e90ef50f7878c38358f0ebf7cdc27a084dd0085
Lubor Budaj <lubor.budaj@epfl.ch>
2024-03-22 14:11:21 +0100

    add description

--- dasklearn/simulation/gossip/__init__.py
+++ dasklearn/simulation/gossip/__init__.py
@@ -1,3 +1,4 @@
 """
-Code related to the gossip algorithm.
+Code related to the Gossip Learning implementation based on the paper by Hegedűs, Danner and Jelasity.
+http://publicatio.bibl.u-szeged.hu/15824/1/dais19a.pdf
 """
