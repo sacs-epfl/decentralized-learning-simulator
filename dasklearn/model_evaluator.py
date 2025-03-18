e5aab6e24a951357e39eecab1c2f79a7ae7c3a00
Martijn de Vos <martijn.devos@epfl.ch>
2025-03-18 11:40:31 +0100

    Unified train/test loss computation

--- dasklearn/model_evaluator.py
+++ dasklearn/model_evaluator.py
@@ -47,18 +47,21 @@ class ModelEvaluator:
         model.to(device)
         model.eval()
 
-        ce_loss = nn.CrossEntropyLoss()
         feature_column_name = "x" if self.settings.dataset == "femnist" else "img"
         label_column_name = "y" if self.settings.dataset == "femnist" else "label"
+
+        if self.settings.dataset == "cifar10":
+            lossf = nn.CrossEntropyLoss() if self.settings.model in ["resnet8", "resnet18", "mobilenet_v3_large"] else nn.NLLLoss()
+        else:
+            lossf = nn.CrossEntropyLoss()
+    
         with torch.no_grad():
             for batch in iter(test_loader):
                 data, target = batch[feature_column_name], batch[label_column_name]
                 data, target = Variable(data.to(device)), Variable(target.to(device))
                 output = model.forward(data)
-                if "ResNet" in model.__class__.__name__:
-                    total_loss += ce_loss(output, target)
-                else:
-                    total_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+                loss = lossf(output, target)
+                total_loss += loss.item()
 
                 pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
                 correct += pred.eq(target.view_as(pred)).sum().item()
@@ -66,5 +69,5 @@ class ModelEvaluator:
                 num_batches += 1
 
         accuracy = float(correct) / float(example_number) * 100.0
-        loss = total_loss / float(example_number)
-        return accuracy, loss
+        average_loss = total_loss / num_batches
+        return accuracy, average_loss
